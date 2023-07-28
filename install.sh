54d2239d4a8479fa640be6b22e7c2182b38a18d5
crunchie <github@kranchie.de>
2023-07-28 12:37:00 +0200

    correction for install.sh

--- install.sh
+++ install.sh
@@ -49,16 +49,6 @@ for file in $(find ~/dotfiles -maxdepth 1 -name ".*" ! -name ".zshrc*" ! -name "
 	fi
 done
 
-# Find all dot files then if the original file exists, create a backup
-# Once backed up to {file}.dtbak symlink the new dotfile in place
-for file in $(find ~ -maxdepth 1 -name ".*" ! -name ".gitconfig" -type f  -printf "%f\n" ); do
-    if [ -e ~/$file ]; then
-        mv -f ~/$file{,.dtbak}
-    fi
-    ln -s ~/dotfiles/$file ~/$file
-	echo "$file linked"
-done
-
 # Unlink or move .zshrc config 
 for file in .zshrc .zshrc-theme
 do
